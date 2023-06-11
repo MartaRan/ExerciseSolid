ebb35b186c5a468d2f2443b8dae998b5a5a74767
MartaRan <36073751+MartaRan@users.noreply.github.com>
2023-06-11 17:17:27 +0200

    Update Product.kt

--- app/src/main/java/com/lyjak/anna/exercisesolid/solid/o/Product.kt
+++ app/src/main/java/com/lyjak/anna/exercisesolid/solid/o/Product.kt
@@ -7,3 +7,10 @@ class Product {
     var type: String? = null
 
 }
+
+//poprawiony kod
+data class Product(
+    var name: String? = null
+    var price: Double = 0.0 //zmienione na liczbę, bo nie wiem co tam było wczesniej
+    var type: String? = null
+)
