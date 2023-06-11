209f21a2a3494cbece51c4eea0ba0f8a5b171561
MartaRan <36073751+MartaRan@users.noreply.github.com>
2023-06-11 17:06:19 +0200

    Update Rectangle.kt

--- app/src/main/java/com/lyjak/anna/exercisesolid/solid/l/Rectangle.kt
+++ app/src/main/java/com/lyjak/anna/exercisesolid/solid/l/Rectangle.kt
@@ -8,3 +8,17 @@ open class Rectangle(open var width: Double = 0.0, open var height: Double = 0.0
         }
 
 }
+
+//poprawiony kod
+
+
+open class Rectangle(var width: Double = 0.0, var height: Double = 0.0) { //usunięto open ze zmiennej oraz zmienieno, dla heigght na 0.0
+
+    val area: Double
+        get() = calculateArea() //dodano metodę calculateArea, która oblicza powierchnię prostokąta
+
+    private fun calculateArea(): Double {
+        return width * height
+    }
+}
+
