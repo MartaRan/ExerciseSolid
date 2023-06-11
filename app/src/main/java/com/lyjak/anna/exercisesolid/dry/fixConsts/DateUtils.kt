2396a8d8dc0d9a1023cef481ba705893a6428472
MartaRan <36073751+MartaRan@users.noreply.github.com>
2023-06-11 15:59:31 +0200

    Update DateUtils.kt

--- app/src/main/java/com/lyjak/anna/exercisesolid/dry/fixConsts/DateUtils.kt
+++ app/src/main/java/com/lyjak/anna/exercisesolid/dry/fixConsts/DateUtils.kt
@@ -10,3 +10,24 @@ class DateUtils {
         return days * 1000 * 60 * 60 * 24
     }
 }
+
+
+
+//poprawiona wersja
+
+class DateUtils {
+    companion object {
+        private const val MILLISECONDS_PER_SECOND = 1000
+        private const val SECONDS_PER_MINUTE = 60
+        private const val MINUTES_PER_HOUR = 60
+        private const val HOURS_PER_DAY = 24
+    }
+
+    fun convertMillisecondsToDays(milliseconds: Long): Long {
+        return milliseconds / (MILLISECONDS_PER_SECOND * SECONDS_PER_MINUTE * MINUTES_PER_HOUR * HOURS_PER_DAY)
+    }
+
+    fun convertDaysToMilliseconds(days: Long): Long {
+        return days * (MILLISECONDS_PER_SECOND * SECONDS_PER_MINUTE * MINUTES_PER_HOUR * HOURS_PER_DAY)
+    }
+}
