d85c3ca31836166bd236bcae7c92484f010469b4
MartaRan <36073751+MartaRan@users.noreply.github.com>
2023-06-11 16:13:51 +0200

    Update number_manager.kt

--- app/src/main/java/com/lyjak/anna/exercisesolid/kiss/number_manager.kt
+++ app/src/main/java/com/lyjak/anna/exercisesolid/kiss/number_manager.kt
@@ -11,3 +11,18 @@ class number_manager(private var mNumber: Int) {
         mNumber = mNumber * value
     }
 }
+
+
+
+//poprawiony kod 
+
+class NumberManager(private var number: Int) {//zmiana nazwy klasy, na pisaną duzymi literami, zmienna zosta…a napisana małymi zgodnie z zasadami 
+
+    companion object {
+        const val oneHundred = 100
+    }
+
+    fun multiplyNumber(value: Int) {. //Zmieniono nazwę metody na multiplyNumber i zastosowano skróconą wersję mnożenia z przypisaniem (number *= value), co jest bardziej zwięzłe i czytelne
+        number *= value 
+    }
+}
