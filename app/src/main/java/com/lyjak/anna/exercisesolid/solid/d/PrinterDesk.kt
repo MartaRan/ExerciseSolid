3f6edd898f7bfd0efc1b6ebe63c510015d79c342
MartaRan <36073751+MartaRan@users.noreply.github.com>
2023-06-11 16:23:06 +0200

    Update PrinterDesk.kt

--- app/src/main/java/com/lyjak/anna/exercisesolid/solid/d/PrinterDesk.kt
+++ app/src/main/java/com/lyjak/anna/exercisesolid/solid/d/PrinterDesk.kt
@@ -7,3 +7,14 @@ class PrinterDesk(private val printer: DotMatrixPrinterPrinter) {
     }
 
 }
+
+
+//poprawiony kod 
+
+class PrinterDesk(private val printer: DotMatrixPrinter) { //usunieto duplikat słowa printer
+
+    fun printFile(fileContent: String) {//zmieniono metodę aby przekazywała zawartość jako string 
+        printer.printText(fileContent)  // a następnie drukowała zawartość przekaznego pliku, a nie jak wcześnej nazwę "File Content
+    }
+
+}
