bc2b1c9e8f659827bfc0c5ec12834162b06a5832
MartaRan <36073751+MartaRan@users.noreply.github.com>
2023-06-11 17:11:09 +0200

    Update Square.kt

--- app/src/main/java/com/lyjak/anna/exercisesolid/solid/l/Square.kt
+++ app/src/main/java/com/lyjak/anna/exercisesolid/solid/l/Square.kt
@@ -15,3 +15,20 @@ class Square : Rectangle() {
         }
 
 }
+
+
+//poprawiony kod
+
+class Square(side: Double) : Rectangle(side, side) {// dodano odnośnil do klasy Rectangle p której dziedziczy Square
+    override var width: Double //W konstruktorze klasy Square przekazano tę samą wartość jako szerokość i wysokość prostokąta, aby utworzyć kwadrat.
+        get() = super.width //Przesłonięto właściwości width i height, aby zapobiec ich modyfikacji dla obiektów typu Square.
+        set(width) {
+            throw IllegalAccessException("Cannot modify width of a square")
+        }
+
+    override var height: Double //W metodzie setter dla właściwości width i height rzucono wyjątek IllegalAccessException, aby wskazać, że szerokość i wysokość kwadratu nie mogą być modyfikowane.
+        get() = super.height
+        set(height) {
+            throw IllegalAccessException("Cannot modify height of a square")
+        }
+}
