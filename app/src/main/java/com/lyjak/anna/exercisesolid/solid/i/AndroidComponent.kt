1f89c8433f78ce0f06759e9a3d708d937952c27e
MartaRan <36073751+MartaRan@users.noreply.github.com>
2023-06-11 16:30:53 +0200

    Update AndroidComponent.kt

--- app/src/main/java/com/lyjak/anna/exercisesolid/solid/i/AndroidComponent.kt
+++ app/src/main/java/com/lyjak/anna/exercisesolid/solid/i/AndroidComponent.kt
@@ -19,3 +19,25 @@ class AndroidComponent : Component {
     }
 
 }
+
+//poprawiony kod
+
+
+class AndroidComponent : Component {
+
+    override fun mouseover(event: String) {
+        throw UnsupportedOperationException("Mouse click not supported")//dodano wielką literę w komunikacie
+    }
+
+    override fun touch(event: String) {
+        println("Touch event fired: $event")//zastosowano odnośnik do event 
+    }
+
+    override fun swipe(event: String) {
+        println("Swipe event fired: $event") //zastosowano odnośnik do event 
+    }
+
+    override fun validate() {
+        println("UI is valid")//zmienio literówkę z "i" na "is" 
+    }
+}
