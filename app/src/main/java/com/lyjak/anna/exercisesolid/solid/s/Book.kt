bc779a6af8e0e8871e20ae76e00d1e3c6ba852d3
MartaRan <36073751+MartaRan@users.noreply.github.com>
2023-06-13 22:18:59 +0200

    Update Book.kt

--- app/src/main/java/com/lyjak/anna/exercisesolid/solid/s/Book.kt
+++ app/src/main/java/com/lyjak/anna/exercisesolid/solid/s/Book.kt
@@ -7,3 +7,17 @@ class Book {
 
     fun print() = println(text)
 }
+
+//poprwiony kod
+
+class Book {
+    var author: String? = null
+    var text: String? = null
+    var name: String? = null
+
+    fun printBookInfo() { //utworzono print book info jako osobna funckję 
+        println("Book Name: $name")
+        println("Author: $author")
+        println("Text: $text")
+    }
+}
