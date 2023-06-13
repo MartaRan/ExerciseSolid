4f4e53d687d48ab38ca0c29aaeab611ab270343f
MartaRan <36073751+MartaRan@users.noreply.github.com>
2023-06-13 22:12:59 +0200

    Update ShoppingCartService.kt

--- app/src/main/java/com/lyjak/anna/exercisesolid/solid/o/ShoppingCartService.kt
+++ app/src/main/java/com/lyjak/anna/exercisesolid/solid/o/ShoppingCartService.kt
@@ -21,25 +21,4 @@ class ShoppingCartService {
 }
 
 //poprawiony kod
-class ShoppingCartService {
-
-    fun calculateTotalOrder(products: List<Product>): Double { //Wydzielono obliczenia dla poszczególnych produktów do prywatnej metody calculateProductTotal,
-                                                               //która przyjmuje pojedynczy produkt i zwraca obliczoną wartość.
-        var orderTotal = 0.0
-
-        for (product in products) {
-            orderTotal += calculateProductTotal(product)
-        }
-
-        return orderTotal
-    }
-
-    private fun calculateProductTotal(product: Product): Double { //wydzielono prywatną funkcję, gdzie można wyliczyć dla każdego produktu inna wartość 
-        return when (product.type) { //Zastosowano instrukcję when do sprawdzania typu produktu. Dzięki temu kod jest bardziej czytelny i eliminuje potrzebę wielokrotnego sprawdzania warunków if-else.
-            "FOOD" -> product.price + product.price * 0.08
-            "CAR" -> product.price * 1.60 * 1.18
-            "Electronics" -> product.price * 1.18
-            else -> product.price
-        }
-    }
-}
+???
