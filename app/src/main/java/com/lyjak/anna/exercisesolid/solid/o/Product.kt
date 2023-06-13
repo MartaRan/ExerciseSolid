f3f3feb71fb60f95283d7d9b4ebff84e814f3050
MartaRan <36073751+MartaRan@users.noreply.github.com>
2023-06-13 22:12:09 +0200

    Update Product.kt

--- app/src/main/java/com/lyjak/anna/exercisesolid/solid/o/Product.kt
+++ app/src/main/java/com/lyjak/anna/exercisesolid/solid/o/Product.kt
@@ -13,4 +13,7 @@ data class Product(
     var name: String? = null
     var price: Double = 0.0 //zmienione na liczbę, bo nie wiem co tam było wczesniej
     var type: String? = null
-)
+ )
+//można byłoby dodać każdą klasę osobno jako open class i byłoby to charakterystyczne dla okreslonego produktu 
+
+
